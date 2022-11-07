4312dd09b5bd50bfc286fc81c9dcac63d9d4ee4e
sonuchoudhary <sonuchoudhary987654@gmail.com>
2022-11-08 00:34:52 +0530

    Added the Cart, Modal and Loader in Project

--- Documents/gfgReact/ecommercewebsite/src/App.js
+++ Documents/gfgReact/ecommercewebsite/src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Products from './components/Products/Products';
 import Header from './components/Layout/header';
 import SubHeader from './components/Layout/subheader';
+import { useState } from 'react';
 function App() {
+  const [cartItems, setCartItems] = useState(0);
+  const handleAddItem = () => {
+    setCartItems(cartItems+1);
+  }
+  const handleRemoveItem = () => {
+    setCartItems(cartItems-1);
+  }
   return (
     <div>
-      <Header />
+      <Header count = {cartItems} />
       <SubHeader />
-      <Products />
+      <Products onAddItem = {handleAddItem} onRemoveItem={handleRemoveItem} />
     </div>
   );
 }
--- Documents/gfgReact/ecommercewebsite/src/components/Products/Products.js
+++ Documents/gfgReact/ecommercewebsite/src/components/Products/Products.js
@@ -1,47 +1,76 @@
-import ListItem from "../ListItems/ListItem";
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Loader from "../UI/Loader";
-const Products = ()  => {
-    const [items,setItems] = useState([]);
-    const [loader, setLoader] = useState(true);
+import ListItem from '../ListItems/ListItem';
+
+const Products = ({ onAddItem, onRemoveItem }) => {
+    const [items, setItems] = useState([])
+    const [loader, setLoader] = useState(true)
+    const [presentItems, setPresentItems] = useState([])
+    //new Add
+
     useEffect(() => {
-        async function fetchAsyncData(){
-            try{
-                const response = await axios.get(`https://ecommerce-2022-d40f1-default-rtdb.firebaseio.com/items.json`);
+        async function fetchItems() {
+            try {
+                const response = await axios.get('https://ecommerce-2022-d40f1-default-rtdb.firebaseio.com/items.json');
                 const data = response.data;
-                const transformData = data.map((item,index) => {
+                const transformedData = data.map((item,index) => {
                     return {
                         ...item,
                         id : index
                     }
                 })
-            //console.log(transformData);
-                //setLoader(false);
-                setItems(transformData);
+                // setLoader(false)
+                setItems(transformedData)   
+            } 
+            catch (error) {
+                // setLoader(false)
+                console.log("Error: ", error)
+                alert("Some error occurred");
             }
-            catch(err){
-                //setLoader(false);
-                console.log(err);
+            finally {
+                setLoader(false)
             }
-            finally{
-                setLoader(false);
-            }
-            
         }
-        fetchAsyncData();
-    },[])
+
+        fetchItems();
+    }, [])
+
+    const handleAddItem = id => {
+        if(presentItems.indexOf(id) > -1) {
+            return;
+        }
+        setPresentItems([...presentItems, id])
+        onAddItem();
+    }
+
+    const handleRemoveItem = id => {
+        let index = presentItems.indexOf(id)
+        if(index > -1) {
+            let items = [...presentItems]
+            items.splice(index, 1)
+            setPresentItems([...items]);
+            onRemoveItem();
+        }
+    }
+
     return (
-    <>
-        <div className = {"product-list"}>
-           <div className = {"product-list--wrapper"}>
-                {items.map(item => {
-                    return (<ListItem key = {item.id} data = {item} />)
-                })}
+        <>
+        <div className={"product-list"}>
+            <div className={"product-list--wrapper"}>
+                {/* <ListItem data={items[0]}></ListItem>
+                <ListItem data={items[1]}></ListItem> */}
+                {
+                    items.map(item => {
+                        return (<ListItem onAdd={handleAddItem} onRemove={handleRemoveItem} key={item.id} data={item}/>)
+                    })
+                }
+                {/* {[<ListItem data={item[0]}/>,<ListItem data={item[1]}/>,<ListItem data={item[3]}/>]} */}
             </div>
         </div>
-        {loader && <Loader />}
-    </>
+        { loader && <Loader/>}
+        </>
     )
 }
+
 export default Products;
