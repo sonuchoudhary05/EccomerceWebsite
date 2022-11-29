090a52c71ddf930bfb754ccbeb34f1c8d84070fc
sonuchoudhary <sonuchoudhary987654@gmail.com>
2022-11-29 20:26:04 +0530

    State centralised by using React Redux

--- Documents/gfgReact/ecommercewebsite/src/components/Cart/Cart.js
+++ Documents/gfgReact/ecommercewebsite/src/components/Cart/Cart.js
@@ -2,23 +2,38 @@ import { Fragment, useState } from "react"
 import Modal from "../UI/Modal"
 import CartItem from "./CartItem";
 import OrderSuccess from "../UI/OrderSuccess";
+import { useDispatch, useSelector } from "react-redux";
+import { addItemHandler, removeItemHandler, clearCartHandler } from "../../actions";
 
-const Cart = ({ count, items, onHandleEvent }) => {
+const Cart = () => {
     const [showModal, setShowModal] = useState(false);
     const [orderModal, setOrderModal] = useState(false);
+    const items = useSelector(state => state.items);
+    const totalAmount = useSelector(state => state.totalAmount);
+    const dispatch = useDispatch();
 
     const handleModal = () => {
         setShowModal(previousState => !previousState)
     }
     const handleOrderModal = () => {
+        dispatch( clearCartHandler())
         setShowModal(false);
         setOrderModal(prev => !prev);
+    }
+
+    const dispatchEvents = (type, item) => {
+        if(type === 1){
+            dispatch(addItemHandler(item))
+        }
+        else if(type === -1){
+            dispatch(removeItemHandler(item.id))
+        }
     }
 
     return (
         <Fragment>
             <button onClick={handleModal}>
-                <span data-items={count}>Cart</span>
+                <span data-items={items.length}>Cart</span>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart-plus" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <circle cx="6" cy="19" r="2" />
@@ -35,13 +50,13 @@ const Cart = ({ count, items, onHandleEvent }) => {
                         <h2>Checkout Modal</h2>
                         <div className="checkout-modal_list">
                             {
-                                count > 0 ?
+                                items.length > 0 ?
                                 items.map(item => {
                                     return (
                                         <CartItem 
                                             data={item} 
-                                            onEmitIncreaseItem={id => onHandleEvent(id, 1)} 
-                                            onEmitDecreaseItem={id => onHandleEvent(id, -1)} 
+                                            onEmitIncreaseItem={item => dispatchEvents(1, item)} 
+                                            onEmitDecreaseItem={item => dispatchEvents(-1, item)} 
                                             key={item.id}
                                         />
                                     )
@@ -51,17 +66,12 @@ const Cart = ({ count, items, onHandleEvent }) => {
                             }
                         </div>
                         { 
-                            count > 0 &&
+                            items.length > 0 &&
                             <div className="checkout-modal_footer">
                                 <div className="totalAmount">
                                     <h4>Total Amount: </h4>
                                     <h4>
-                                        {
-                                            items.reduce((previous, current) => {
-                                                return previous + (current.discountedPrice * current.quantity)
-                                            }, 0)
-                                        }
-                                        <span style={{marginLeft: "4px"}}>INR</span>
+                                        {totalAmount}
                                     </h4>
                                 </div>
                                 <button onClick = {handleOrderModal}>Order Now</button>
--- Documents/gfgReact/ecommercewebsite/src/index.js
+++ Documents/gfgReact/ecommercewebsite/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.scss"
+import "./index.scss";
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
